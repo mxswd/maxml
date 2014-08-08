29e65e645f46c2b2d907f549dee4dcbd3eebe2d3
Maxwell Swadling <maxwellswadling@gmail.com>
2014-08-08 20:11:35 +1000

    added MaxML library

---
+++ Data/MaxML.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances #-}
+module Data.MaxML where
+
+-- | The definition of MaxML
+class (Read a, Show a) => MaxML a
+
+-- | Haskell has a lot of MaxML compatible types.
+instance (Read a, Show a) => MaxML a
+
+-- | a proposition that a type is in MaxML
+isMaxML :: MaxML a => a -> ()
+isMaxML a = ()
